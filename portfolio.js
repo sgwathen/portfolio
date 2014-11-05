bbbf5fe554fc59054769b46db90d64d302576614
sgwathen <samuelwathen@MAC.local>
2014-11-05 03:28:21 -0600

    interactive

--- portfolio.js
+++ portfolio.js
@@ -5,20 +5,21 @@ $(document).ready(function(){
     		var href = $.attr(this, 'href');
     			$root.animate({
         		scrollTop: $(href).offset().top
-    			}, 1000, function () {
+    			}, 1500, function () {
         		window.location.hash = href;
     		});
     	return false;
 		});
 	}
-	$('nav li a').click(function(e){
-		e.preventDefault();
-		softScrolling();
-	});
-	$('.arrow').click(function(e){
-		e.preventDefault();
-		softScrolling();
-	});
+	softScrolling();
+//	$('nav li a').click(function(e){
+//		e.preventDefault();
+//		softScrolling();
+//	});
+//	$('.arrow').click(function(e){
+//		e.preventDefault();
+//		softScrolling();
+	//});
 	//displays portfolio overlay on mouseenter
 	$('#portcont').mouseenter(function(){
 		$('.overlay').fadeIn(1000);
